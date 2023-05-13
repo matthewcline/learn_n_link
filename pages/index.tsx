93aa8d9d3b6d4136f7b3c4ef2f486ab77897e519
Will Daugherty <wld37@cornell.edu>
2023-05-13 14:02:28 -0400

    Different question sets

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,6 +14,7 @@ const Home: NextPage = () => {
   const [bio, setBio] = useState("");
   const [vibe, setVibe] = useState<VibeType>("Professional");
   const [generatedBios, setGeneratedBios] = useState<String>("");
+  const [questionSet, setQuestionSet] = useState("GenerateLeads");
 
   const bioRef = useRef<null | HTMLDivElement>(null);
 
@@ -21,6 +22,16 @@ const Home: NextPage = () => {
     if (bioRef.current !== null) {
       bioRef.current.scrollIntoView({ behavior: "smooth" });
     }
+  };
+
+  const questionSet1 = {
+    bioQuestion: "What experience do you want to highlight?",
+    connectQuestion: "Who are you looking to connect with!",
+  };
+
+  const questionSet2 = {
+    bioQuestion: "What is your favorite hobby?",
+    connectQuestion: "What is your dream job?",
   };
 
   const prompt = `Generate 2 ${vibe} twitter biographies with no hashtags and clearly labeled "1." and "2.". ${
@@ -80,28 +91,48 @@ const Home: NextPage = () => {
       <Header />
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Find the perfect intro to the people and jobs you want to connect with.
+          Find the perfect intro to the people and jobs you want to connect with!
         </h1>
         <div className="max-w-xl w-full">
+          <div className="flex justify-center mt-8">
+            <button
+              className={`px-4 py-2 rounded-md mx-2 ${
+                questionSet === "GenerateLeads"
+                  ? "bg-black text-white"
+                  : "bg-gray-300 text-gray-700"
+              }`}
+              onClick={() => setQuestionSet("GenerateLeads")}
+            >
+              Generate Leads
+            </button>
+            <button
+              className={`px-4 py-2 rounded-md mx-2 ${
+                questionSet === "WriteCover"
+                  ? "bg-black text-white"
+                  : "bg-gray-300 text-gray-700"
+              }`}
+              onClick={() => setQuestionSet("WriteCover")}
+            >
+              Write Cover Letters
+            </button>
+          </div>
           <div className="flex mt-10 items-center space-x-3">
             <h1 className="text-2xl">🔁</h1>
-            <p className="text-left font-medium">
-              Connect your LinkedIn
-            </p>
+            <p className="text-left font-medium">Connect your LinkedIn</p>
           </div>
           <textarea
             value={bio}
             onChange={(e) => setBio(e.target.value)}
             rows={1}
             className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
-            placeholder={
-              "https://www.linkedin.com/in/yourname"
-            }
+            placeholder={"https://www.linkedin.com/in/yourname"}
           />
           <div className="flex items-center space-x-3">
             <h1 className="text-2xl">👩‍💻</h1>
             <p className="text-left font-medium">
-              What experience do you want to highlight?
+              {questionSet === "GenerateLeads"
+                ? questionSet1.bioQuestion
+                : questionSet2.bioQuestion}
             </p>
           </div>
           <textarea
@@ -116,7 +147,9 @@ const Home: NextPage = () => {
           <div className="flex items-center space-x-3">
             <h1 className="text-2xl">🤝</h1>
             <p className="text-left font-medium">
-              Who are you looking to connect with?
+              {questionSet === "GenerateLeads"
+                ? questionSet1.connectQuestion
+                : questionSet2.connectQuestion}
             </p>
           </div>
           <textarea
@@ -128,20 +161,12 @@ const Home: NextPage = () => {
               "e.g. Engineering managers, recruiters, and developers at companies like Google, Facebook, and Amazon."
             }
           />
-          {/* <div className="flex mb-5 items-center space-x-3">
-            <Image src="/2-black.png" width={30} height={30} alt="1 icon" />
-            <p className="text-left font-medium">Select your vibe.</p>
-          </div>
-          <div className="block">
-            <DropDown vibe={vibe} setVibe={(newVibe) => setVibe(newVibe)} />
-          </div> */}
-
           {!loading && (
             <button
               className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
               onClick={(e) => generateBio(e)}
             >
-              Generate your leads &rarr;
+              Generate your leads →
             </button>
           )}
           {loading && (
@@ -154,50 +179,50 @@ const Home: NextPage = () => {
           )}
         </div>
         <Toaster
-          position="top-center"
-          reverseOrder={false}
-          toastOptions={{ duration: 2000 }}
-        />
-        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
-        <div className="space-y-10 my-10">
-          {generatedBios && (
-            <>
-              <div>
-                <h2
-                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
-                  ref={bioRef}
-                >
-                  Your generated bios
-                </h2>
-              </div>
-              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
-                {generatedBios
-                  .substring(generatedBios.indexOf("1") + 3)
-                  .split("2.")
-                  .map((generatedBio) => {
-                    return (
-                      <div
-                        className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
-                        onClick={() => {
-                          navigator.clipboard.writeText(generatedBio);
-                          toast("Bio copied to clipboard", {
-                            icon: "✂️",
-                          });
-                        }}
-                        key={generatedBio}
+                position="top-center"
+                reverseOrder={false}
+                toastOptions={{ duration: 2000 }}
+              />
+              <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+              <div className="space-y-10 my-10">
+                {generatedBios && (
+                  <>
+                    <div>
+                      <h2
+                        className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
+                        ref={bioRef}
                       >
-                        <p>{generatedBio}</p>
-                      </div>
-                    );
-                  })}
+                        Your generated bios
+                      </h2>
+                    </div>
+                    <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                      {generatedBios
+                        .substring(generatedBios.indexOf("1") + 3)
+                        .split("2.")
+                        .map((generatedBio) => {
+                          return (
+                            <div
+                              className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                              onClick={() => {
+                                navigator.clipboard.writeText(generatedBio);
+                                toast("Bio copied to clipboard", {
+                                  icon: "✂️",
+                                });
+                              }}
+                              key={generatedBio}
+                            >
+                              <p>{generatedBio}</p>
+                            </div>
+                          );
+                        })}
+                    </div>
+                  </>
+                )}
               </div>
-            </>
-          )}
-        </div>
-      </main>
-      {/* <Footer /> */}
-    </div>
-  );
-};
-
-export default Home;
+            </main>
+            {/* <Footer /> */}
+          </div>
+        );
+      };
+      
+      export default Home;
