217b7cfdeee37d1d4e09200c2e7e2dc65889c1e1
Matthew Cline <clinemattw@gmail.com>
2023-05-13 16:41:08 -0400

    add edit bar to lead
    add images
    
    style changes

--- components/EmojiCard.tsx
+++ components/EmojiCard.tsx
@@ -2,12 +2,13 @@
 interface EmojiCardProps {
   emoji: string;
   text: string;
+  color: string;
 }
 
-export default function EmojiCard({ emoji, text }: EmojiCardProps) {
+export default function EmojiCard({ emoji, text, color }: EmojiCardProps) {
   return (
     <div
-      className="bg-white rounded-xl shadow-md p-4 mr-4 mb-4 hover:bg-gray-100 transition cursor-copy"
+      className={`border-2 border-${color}-300 rounded-xl p-4 mr-4 mb-4`}
       key={text}
     >
       {emoji} {text}
